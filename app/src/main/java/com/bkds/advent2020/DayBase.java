0d938e3fee2872768c4b746f57f28b8eaa5ea32c
Brian Krisler <bkrisler@bkds.com>
2020-12-04 10:40:43 -0500

    Add some documentation on how to proceed

--- app/src/main/java/com/bkds/advent2020/DayBase.java
+++ app/src/main/java/com/bkds/advent2020/DayBase.java
@@ -1,6 +1,5 @@
 package com.bkds.advent2020;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,11 +7,17 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public abstract class DayBase {
-	
+
+	/**
+	 * Called on each data input row received. This allows us
+	 * to store the input as we need for each day.
+	 * 
+	 * @param input A single line of data from the input
+	 */
 	public abstract void store(String input);
-	
+
 	protected void readData(String day, String person) {
-		System.out.println("Reading in data for: " + person);
+		System.out.println("Reading in data for: " + person + " and " + day);
 		InputStream is = this.getFileAsResource("data/" + day + "/" + person + ".txt");
 		try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
 				BufferedReader reader = new BufferedReader(streamReader)) {
@@ -29,10 +34,10 @@ public abstract class DayBase {
 	private InputStream getFileAsResource(String filename) {
 		ClassLoader classLoader = getClass().getClassLoader();
 		InputStream stream = classLoader.getResourceAsStream(filename);
-		if(stream == null) {
+		if (stream == null) {
 			throw new IllegalArgumentException("Unknown file: " + filename);
-		} 
-		
-		return stream;		
-	}	
+		}
+
+		return stream;
+	}
 }
--- app/src/main/java/com/bkds/advent2020/day3/Day3.java
+++ app/src/main/java/com/bkds/advent2020/day3/Day3.java
@@ -5,16 +5,16 @@ import com.bkds.advent2020.DayBase;
 public class Day3 extends DayBase {
 
 	public Day3(String person) {
-		// TODO Auto-generated constructor stub
 		readData("day3", person);
 	}
 
 	@Override
 	public void store(String input) {
-		// TODO Auto-generated method stub
-		
+		// Step 2: Store the input data into the desired data structure.
 	}
 
+	// Step 3: Create a method for answering the first part of the problem.
+	
 	public static void main(String[] args) {
 		if(args.length == 0) {
 			System.err.println("Add person name as an argument!");
@@ -22,6 +22,7 @@ public class Day3 extends DayBase {
 		}
 		String person = args[0];
 
+		// Step 1: Construct a new class for testing and call the "solve" methods
 	}
 
 }
