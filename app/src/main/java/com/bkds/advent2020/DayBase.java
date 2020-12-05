adf27a30c475b69fbbd6651612ad7fb121ee30b4
mae <mae@bkds.com>
2020-12-05 12:46:18 -0500

    day 4 solved!

--- app/src/main/java/com/bkds/advent2020/DayBase.java
+++ app/src/main/java/com/bkds/advent2020/DayBase.java
@@ -6,62 +6,85 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public abstract class DayBase {
+public abstract class DayBase
+{
 
-	/**
-	 * Called on each data input row received. This allows us
-	 * to store the input as we need for each day.
-	 * 
-	 * @param input A single line of data from the input
-	 */
-	public abstract void store(String input);
+  /**
+   * Called on each data input row received. This allows us
+   * to store the input as we need for each day.
+   * 
+   * @param input
+   *          A single line of data from the input
+   */
+  public abstract void store(String input);
 
-	protected void readData(String day, String person) {
-		System.out.println("Reading in data for: " + person + " and " + day);
-		InputStream is = this.getFileAsResource("data/" + day + "_" + person + ".txt");
-		try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
-				BufferedReader reader = new BufferedReader(streamReader)) {
+  protected void readData(String day, String person)
+  {
+    System.out.println("Reading in data for: " + person + " and " + day);
+    InputStream is =
+      this.getFileAsResource("data/" + day + "_" + person + ".txt");
+    try (
+      InputStreamReader streamReader =
+        new InputStreamReader(is, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(streamReader))
+    {
 
-			String line;
-			while ((line = reader.readLine()) != null) {
-				store(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      String line;
+      while ((line = reader.readLine()) != null)
+      {
+        store(line);
+      }
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	protected void readDataMultiRow(String day, String person) {
-		System.out.println("Reading in data for: " + person + " and " + day);
-		String entry = "";
-		InputStream is = this.getFileAsResource("data/" + day + "_" + person + ".txt");
-		try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
-				BufferedReader reader = new BufferedReader(streamReader)) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.print(line + " ");
-				if(line.isEmpty()) {
-					System.out.println("");
-					store(entry);
-					entry = "";
-				} else {
-					entry += line;
-				}
-			}
-			store(entry);
-			System.out.println("");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  protected void readDataMultiRow(String day, String person)
+  {
+    System.out.println("Reading in data for: " + person + " and " + day);
+    String entry = "";
+    InputStream is =
+      this.getFileAsResource("data/" + day + "_" + person + ".txt");
+    try (
+      InputStreamReader streamReader =
+        new InputStreamReader(is, StandardCharsets.UTF_8);
+      BufferedReader reader = new BufferedReader(streamReader))
+    {
+      String line;
+      while ((line = reader.readLine()) != null)
+      {
+        System.out.print(line + " ");
+        if (line.isEmpty())
+        {
+          System.out.println("");
+          store(entry);
+          entry = "";
+        }
+        else
+        {
+          entry += line + " ";
+        }
+      }
+      store(entry);
+      System.out.println("");
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	private InputStream getFileAsResource(String filename) {
-		ClassLoader classLoader = getClass().getClassLoader();
-		InputStream stream = classLoader.getResourceAsStream(filename);
-		if (stream == null) {
-			throw new IllegalArgumentException("Unknown file: " + filename);
-		}
+  private InputStream getFileAsResource(String filename)
+  {
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream stream = classLoader.getResourceAsStream(filename);
+    if (stream == null)
+    {
+      throw new IllegalArgumentException("Unknown file: " + filename);
+    }
 
-		return stream;
-	}
+    return stream;
+  }
 }
--- app/src/main/java/com/bkds/advent2020/day4/Day4.java
+++ app/src/main/java/com/bkds/advent2020/day4/Day4.java
@@ -1,39 +1,212 @@
 package com.bkds.advent2020.day4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.bkds.advent2020.DayBase;
 
-public class Day4 extends DayBase {
+public class Day4 extends DayBase
+{
 
-	private List<Map<String, String>> passports = new ArrayList<>();
-	
-	public Day4() {
-		readDataMultiRow("day4", "brian");
-		dumpPassports();
-	}
+  private List<Map<String, String>> passports = new ArrayList<>();
 
-	private void dumpPassports() {
-		for(Map<String,String> passport : passports) {
-			System.out.println(passport);
-		}
-	}
-	
-	@Override
-	public void store(String input) {
-		Map<String, String> eMap = new HashMap<>();
-		String[] fields = input.split(" ");
-		for(String field : fields) {
-			String[] fieldParts = field.split(":");
-			eMap.put(fieldParts[0], fieldParts[1]);
-		}
-		passports.add(eMap);
-	}
+  public Day4()
+  {
+    readDataMultiRow("day4", "mae");
+    dumpPassports();
+    System.out.println(passports.size());
+  }
 
-	public static void main(String[] args) {
-		Day4 d4 = new Day4();
-	}
+  public void checkValidPassports()
+  {
+    int validPassportCount = 0;
+    for (Map<String, String> passport : passports)
+    {
+      Set<String> fields = passport.keySet();
+
+      if (fields.contains("hcl") && fields.contains("byr")
+        && fields.contains("iyr") && fields.contains("eyr")
+        && fields.contains("hgt") && fields.contains("ecl")
+        && fields.contains("pid") && fields.contains("cid"))
+      {
+        validPassportCount++;
+      }
+      else if (fields.contains("hcl") && fields.contains("byr")
+        && fields.contains("iyr") && fields.contains("eyr")
+        && fields.contains("hgt") && fields.contains("ecl")
+        && fields.contains("pid"))
+      {
+
+        validPassportCount++;
+      }
+    }
+    System.out.println(validPassportCount);
+
+  }
+
+  public void checkValidDataSets()
+  {
+    int validPassportCount = 0;
+    for (Map<String, String> passport : passports)
+    {
+      Set<String> fields = passport.keySet();
+
+      if (fields.contains("hcl") && fields.contains("byr")
+        && fields.contains("iyr") && fields.contains("eyr")
+        && fields.contains("hgt") && fields.contains("ecl")
+        && fields.contains("pid") && fields.contains("cid"))
+      {
+        if (valid(passport))
+        {
+          validPassportCount++;
+        }
+      }
+      else if (fields.contains("hcl") && fields.contains("byr")
+        && fields.contains("iyr") && fields.contains("eyr")
+        && fields.contains("hgt") && fields.contains("ecl")
+        && fields.contains("pid"))
+      {
+        if (valid(passport))
+        {
+          validPassportCount++;
+        }
+      }
+    }
+    System.out.println(validPassportCount);
+
+  }
+
+  private boolean valid(Map<String, String> passport)
+  {
+    boolean isbyr = false;
+    boolean isiyr = false;
+    boolean iseyr = false;
+    boolean ishgt = false;
+    boolean ishcl = false;
+    boolean isecl = false;
+    boolean ispid = false;
+
+    Integer bDayYear = Integer.valueOf(passport.get("byr"));
+    if (bDayYear >= 1920 && bDayYear <= 2002)
+    {
+      isbyr = true;
+    }
+    else
+    {
+      isbyr = false;
+    }
+    Integer iyr = Integer.valueOf(passport.get("iyr"));
+    if (iyr >= 2010 && iyr <= 2020)
+    {
+      isiyr = true;
+    }
+    else
+    {
+      isiyr = false;
+    }
+    Integer eyr = Integer.valueOf(passport.get("eyr"));
+    if (eyr >= 2020 && eyr <= 2030)
+    {
+      iseyr = true;
+    }
+    else
+    {
+      iseyr = false;
+    }
+    String hgt = passport.get("hgt");
+    if (hgt.endsWith("cm"))
+    {
+      Integer temp = Integer.valueOf(hgt.substring(0, hgt.length() - 2));
+      if (temp <= 193 && temp >= 150)
+      {
+        ishgt = true;
+      }
+    }
+    else if (hgt.endsWith("in"))
+    {
+      Integer temp = Integer.valueOf(hgt.substring(0, hgt.length() - 2));
+      if (temp <= 76 && temp >= 59)
+      {
+        ishgt = true;
+      }
+    }
+    else
+    {
+      ishgt = false;
+    }
+    String hcl = passport.get("hcl");
+    if (hcl.startsWith("#") && hcl.length() == 7)
+    {
+      List<String> req =
+        Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a",
+                      "b", "c", "d", "e", "f");
+      String[] parts = hcl.substring(1).split("");
+      ishcl = true;
+      for (String part : parts)
+      {
+        if (!req.contains(part))
+        {
+          ishcl = false;
+          break;
+        }
+      }
+    }
+    else
+    {
+      ishcl = false;
+    }
+    String ecl = passport.get("ecl");
+    if (ecl.equals("amb") || ecl.equals("blu") || ecl.equals("brn")
+      || ecl.equals("gry") || ecl.equals("grn") || ecl.equals("hzl")
+      || ecl.equals("oth"))
+    {
+      isecl = true;
+    }
+    else
+    {
+      isecl = false;
+    }
+    String pid = passport.get("pid");
+    if (pid.length() == 9)
+    {
+      ispid = true;
+    }
+    else
+    {
+      ispid = false;
+    }
+    return isbyr && isiyr && iseyr && ishgt && ishcl && isecl && ispid;
+  }
+
+  private void dumpPassports()
+  {
+    for (Map<String, String> passport : passports)
+    {
+      System.out.println(passport);
+    }
+  }
+
+  @Override
+  public void store(String input)
+  {
+    Map<String, String> eMap = new HashMap<>();
+    String[] fields = input.split(" ");
+    for (String field : fields)
+    {
+      String[] fieldParts = field.split(":");
+      eMap.put(fieldParts[0], fieldParts[1]);
+    }
+    passports.add(eMap);
+  }
+
+  public static void main(String[] args)
+  {
+    Day4 d4 = new Day4();
+    // d4.checkValidPassports();
+    d4.checkValidDataSets();
+  }
 }
