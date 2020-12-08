f5696dfd0a49ff381b15b386d3ac048fe63ea74a
mae <mae@bkds.com>
2020-12-07 21:25:56 -0500

    mae

--- app/src/main/java/com/bkds/advent2020/day7/Day7Brian.java
+++ app/src/main/java/com/bkds/advent2020/day7/Day7Brian.java
@@ -9,132 +9,157 @@ import java.util.Map;
 import com.bkds.advent2020.DayBase;
 import com.bkds.advent2020.Tree;
 
-public class Day7Brian extends DayBase {
+public class Day7Brian extends DayBase
+{
 
-	private Map<String, List<String>> bags = new HashMap<>();
-	private Tree<BagNode> bagTree = new Tree<>(new BagNode("", 0));
-	private int count = 0;
-	private List<String> colors = new ArrayList<>();
-	
-	public Day7Brian() {
-		readData("day7", "brian");
-		populate(bagTree, new ArrayList<>(bags.keySet()));
-	}
+  private Map<String, List<String>> bags = new HashMap<>();
+  private Tree<BagNode> bagTree = new Tree<>(new BagNode("", 0));
+  private int count = 0;
+  private List<String> colors = new ArrayList<>();
 
-	@Override
-	public void store(String input) {
-		String[] part1 = input.split(" contain ");
+  public Day7Brian()
+  {
+    readData("day7", "mae");
+    populate(bagTree, new ArrayList<>(bags.keySet()));
+  }
 
-		// Get the Bag Color from input
-		String bagColor = part1[0].trim();
-		if (bagColor.endsWith("s")) {
-			// Strip off the s
-			bagColor = bagColor.substring(0, bagColor.length() - 1);
-		}
+  @Override
+  public void store(String input)
+  {
+    String[] part1 = input.split(" contain ");
 
-		// Get the outer bags now
-		List<String> outerBagList = new ArrayList<>();
-		if (!(part1[1].equals("no other bags."))) {
-			String[] outerBags = part1[1].split(",");
-			for (String bag : outerBags) {
-				bag = bag.trim();
-				if (bag.endsWith(".")) {
-					bag = bag.substring(0, bag.length() - 1);
-				}
+    // Get the Bag Color from input
+    String bagColor = part1[0].trim();
+    if (bagColor.endsWith("s"))
+    {
+      // Strip off the s
+      bagColor = bagColor.substring(0, bagColor.length() - 1);
+    }
 
-				String count = bag.substring(0, 1);
-				String pColor = bag.substring(1, bag.length()).trim();
-				if (pColor.endsWith("s")) {
-					// Strip off the s
-					pColor = pColor.substring(0, pColor.length() - 1);
-				}
+    // Get the outer bags now
+    List<String> outerBagList = new ArrayList<>();
+    if (!(part1[1].equals("no other bags.")))
+    {
+      String[] outerBags = part1[1].split(",");
+      for (String bag : outerBags)
+      {
+        bag = bag.trim();
+        if (bag.endsWith("."))
+        {
+          bag = bag.substring(0, bag.length() - 1);
+        }
 
-				BagNode cNode = new BagNode(pColor, Integer.valueOf(count));
-				outerBagList.add(cNode.getColor());
-			}
-		}
-		bags.put(bagColor, outerBagList);
-	}
+        String count = bag.substring(0, 1);
+        String pColor = bag.substring(1, bag.length()).trim();
+        if (pColor.endsWith("s"))
+        {
+          // Strip off the s
+          pColor = pColor.substring(0, pColor.length() - 1);
+        }
 
-	private void populate(Tree<BagNode> parent, List<String> children) {
-		for (String color : children) {
-			Tree<BagNode> child = parent.addChild(new BagNode(color, 0));
-			if (!bags.get(color).isEmpty()) {
-				populate(child, bags.get(color));
-			}
-		}
-	}
-	
-	private String walkTree(Tree<BagNode> node, String path, int space) {
+        BagNode cNode = new BagNode(pColor, Integer.valueOf(count));
+        outerBagList.add(cNode.getColor());
+      }
+    }
+    bags.put(bagColor, outerBagList);
+  }
 
-		if(node.getChildren().size() == 0) {
-			System.out.println(path);
-			System.out.println("");
-			System.out.println("--> " + node.getValue().getColor());
-			count++;
-		}
-		
-		for(Tree<BagNode> child : node.getChildren()) {
-			String fwd = path;
-			fwd += "\n";
-			for(int i=0; i < space; i++) {
-				fwd += " ";
-			}
-			fwd += "\u2515 ";
-			fwd += child.getValue().getColor();
-			walkTree(child, fwd, (space + 2));
-		}
+  private void populate(Tree<BagNode> parent, List<String> children)
+  {
+    for (String color : children)
+    {
+      Tree<BagNode> child = parent.addChild(new BagNode(color, 0));
+      if (!bags.get(color).isEmpty())
+      {
+        populate(child, bags.get(color));
+      }
+    }
+  }
 
-		return node.getValue().getColor();
-	}
+  private String walkTree(Tree<BagNode> node, String path, int space)
+  {
 
-	private void walkTreeFlat(Tree<BagNode> node, String path, int space) {
-		
-		if(node.getChildren().size() == 0) {
-			String[] cp = node.getValue().getColor().split(" ");			
-			colors.add(cp[1]);
-			System.out.println(path);
-			count++;
-		}
-		
-		for(Tree<BagNode> child : node.getChildren()) {
-			String fwd = path + " > ";
-			fwd += "[" + child.getValue().getColor() + "]";
-			String[] cp = node.getValue().getColor().split(" ");			
-			colors.add(cp[1]);
-			walkTreeFlat(child, fwd, (space + 2));
-		}
-	}
+    if (node.getChildren().size() == 0)
+    {
+      System.out.println(path);
+      System.out.println("");
+      System.out.println("--> " + node.getValue().getColor());
+      count++;
+    }
 
-	private void getPaths(Tree<BagNode> node, String root) {
-		for(Tree<BagNode> bagNode : bagTree.getChildren()) {
-			if(bagNode.getValue().getColor().equals(root)) {
-//				walkTree(bagNode, root, 2);
-				walkTreeFlat(bagNode, "[" + root + "]", 2);
-				break;
-			}
-		}
-	}
+    for (Tree<BagNode> child : node.getChildren())
+    {
+      String fwd = path;
+      fwd += "\n";
+      for (int i = 0; i < space; i++)
+      {
+        fwd += " ";
+      }
+      fwd += "\u2515 ";
+      fwd += child.getValue().getColor();
+      walkTree(child, fwd, (space + 2));
+    }
 
-	public void solvePartOne(String bag) {
-		getPaths(bagTree, bag);
-		System.out.println("Total: " + count);
-		List<String> unique = new ArrayList<>(new HashSet<>(colors));
-		if(unique.contains(bag)) {
-			unique.remove(bag);
-		}
+    return node.getValue().getColor();
+  }
 
-		if(colors.contains(bag)) {
-			colors.remove(bag);
-		}
+  private void walkTreeFlat(Tree<BagNode> node, String path, int space)
+  {
 
-		System.out.println("All Colors: [" + colors.size() + "] " + colors);
-		System.out.println("Unique Colors: [" + unique.size() + "] " + unique);
-		
-	}
+    if (node.getChildren().size() == 0)
+    {
+      String[] cp = node.getValue().getColor().split(" ");
+      colors.add(cp[1]);
+      System.out.println(path);
+      count++;
+    }
 
-	public static void main(String[] args) {
-		Day7Brian d7b = new Day7Brian();
-		d7b.solvePartOne("shiny gold bag");
-	}
+    for (Tree<BagNode> child : node.getChildren())
+    {
+      String fwd = path + " > ";
+      fwd += "[" + child.getValue().getColor() + "]";
+      String[] cp = node.getValue().getColor().split(" ");
+      colors.add(cp[1]);
+      walkTreeFlat(child, fwd, (space + 2));
+    }
+  }
+
+  private void getPaths(Tree<BagNode> node, String root)
+  {
+    for (Tree<BagNode> bagNode : bagTree.getChildren())
+    {
+      if (bagNode.getValue().getColor().equals(root))
+      {
+        // walkTree(bagNode, root, 2);
+        walkTreeFlat(bagNode, "[" + root + "]", 2);
+        break;
+      }
+    }
+  }
+
+  public void solvePartOne(String bag)
+  {
+    getPaths(bagTree, bag);
+    System.out.println("Total: " + count);
+    List<String> unique = new ArrayList<>(new HashSet<>(colors));
+    if (unique.contains(bag))
+    {
+      unique.remove(bag);
+    }
+
+    if (colors.contains(bag))
+    {
+      colors.remove(bag);
+    }
+
+    System.out.println("All Colors: [" + colors.size() + "] " + colors);
+    System.out.println("Unique Colors: [" + unique.size() + "] " + unique);
+
+  }
+
+  public static void main(String[] args)
+  {
+    Day7Brian d7b = new Day7Brian();
+    d7b.solvePartOne("shiny gold bag");
+  }
 }
