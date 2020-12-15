268892a07dd032a300b474bb1df56a44ab6d27a6
Brian Krisler <bkrisler@bkds.com>
2020-12-14 22:26:03 -0500

    Solved part 2

--- app/src/main/java/com/bkds/advent2020/day14/Day14Brian.java
+++ app/src/main/java/com/bkds/advent2020/day14/Day14Brian.java
@@ -1,6 +1,7 @@
 package com.bkds.advent2020.day14;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -90,30 +91,65 @@ public class Day14Brian extends DayBase {
 			int pos = ((mask.length()-i)-1);
 			System.out.println("--[ " + pos + " ]--[ " + (Long.SIZE - Long.numberOfLeadingZeros(result)) + " ]--");			
 			if(mask.charAt(i) == '0') {
-				System.out.println("Before: " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
-				mark(mask, i);
-				System.out.println("Mask:   " + (mask));		
+				beforePrint(mask, result, i);		
 				result &= ~(1L << pos);
-				mark(mask, i);
-				System.out.println("After:  " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
+				afterPrint(mask, result, i);
 			} else if(mask.charAt(i) == '1') {				
-				System.out.println("Before: " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
-				mark(mask, i);
-				System.out.println("Mask:   " + (mask));		
+				beforePrint(mask, result, i);		
 				result |=  (1L << pos);				
-				mark(mask, i);
-				System.out.println("After:  " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
+				afterPrint(mask, result, i);
 			} else {
-				System.out.println("Before: " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
-				mark(mask, i);
-				System.out.println("Mask:   " + (mask));		
-				mark(mask, i);
-				System.out.println("After:  " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));				
+				beforePrint(mask, result, i);		
+				afterPrint(mask, result, i);				
 			}
 		}
 		System.out.println();
 		System.out.println("Result: (" + result + ") " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
 		return result;
+	}
+	
+
+	public void applyMask2Address(Long memoryLocation, String mask, Long value) {		
+//		System.out.println("Mask:          " + mask);
+//		System.out.println("Location: (" + memoryLocation + ") " + String.format("%36s", Long.toBinaryString(memoryLocation)).replace(' ', '0'));
+//		System.out.println();
+		long result = Long.valueOf(memoryLocation);
+		String[] memAsStr = String.format("%36s", Long.toBinaryString(memoryLocation)).replace(' ', '0').split("");
+		for(int i=mask.length()-1; i >=0; i--) {
+			int pos = ((mask.length()-i)-1);	
+			if(mask.charAt(i) == '0') {
+				//beforePrint(mask, result, i);		
+				result &= ~(1L << pos);
+				//afterPrint(mask, result, i);
+			} else if(mask.charAt(i) == '1') {				
+				//beforePrint(mask, result, i);		
+				result |= (1L << pos);				
+				//afterPrint(mask, result, i);
+			} else if(mask.charAt(i) == '2') {
+				String keep = memAsStr[i];
+				//beforePrint(mask, result, i);
+				if(keep.equals("1")) {							
+					//result |= (1L << pos);	
+				}
+				//afterPrint(mask, result, i);
+			}
+		}		
+		//System.out.println("Result: (" + result + ")   " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
+		memory.put(result, value);
+//		System.out.println("Location == " + result + " with value: " + value);
+//		System.out.println();
+	}
+
+	private void afterPrint(String mask, long result, int i) {
+		mark(mask, i);
+		System.out.println("After:  " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
+		System.out.println();
+	}
+
+	private void beforePrint(String mask, long result, int i) {
+		System.out.println("Before: " + String.format("%36s", Long.toBinaryString(result)).replace(' ', '0'));
+		mark(mask, i);
+		System.out.println("Mask:   " + (mask));
 	}
 	
 	public void testMask(long value) {
@@ -128,40 +164,104 @@ public class Day14Brian extends DayBase {
 		
 	}
 	
+	private Map<Long, Long> memory = new HashMap<>();
+	
+	public void processFloating(Long memoryLocation, String mask, Long value) {
+		//System.out.println("Starting Mask: " + mask);
+		List<String> allMasks = generateMasks(mask);		
+		for(String aMask : allMasks) {
+			//System.out.println(aMask);
+			applyMask2Address(memoryLocation, aMask, value);
+		}	
+		System.out.println();
+	}
+		
 	public void solvePartTwo() {
-		Map<Integer, Long> memoryMap = new HashMap<>();
 		String currentMask = "";
 		for (String input : inputData) {
 			if (input.startsWith("mask")) {
 				currentMask = input.split("=")[1].trim();
 			} else if (input.startsWith("mem")) {
 				String pos = input.substring(4, input.indexOf("]"));
-				int value = Integer.valueOf(input.split("=")[1].trim());
-				memoryMap.put(Integer.valueOf(pos), applyMask(value, currentMask));
+				Long memoryLocation = Long.valueOf(pos);
+				long value = Long.valueOf(input.split("=")[1].trim());
+				//memory.put(memoryLocation, value);
+				//System.out.println("Current Mask:  " + currentMask);
+				processFloating(memoryLocation, currentMask, value);
 			}
 		}
-		System.out.println(memoryMap);
 		
-		long sum = memoryMap.values().stream().collect(Collectors.summingLong(Long::longValue));
+		//System.out.println(memory);
+		long sum = memory.values().stream().collect(Collectors.summingLong(Long::longValue));
 		print("Result = " + sum);
 	}
 	
+	static void printTheArray(int arr[], int n) 
+	{ 
+	    for (int i = 0; i < n; i++)  
+	    { 
+	        System.out.print(arr[i]+" "); 
+	    } 
+	    System.out.println(); 
+	} 
+	
+	// Function to generate all binary strings 
+	static void generateAllBinaryStrings(String mask[], int n,  
+	                            int arr[], int i, List<String> maskList) 
+	{ 
+	    if (i == n)  
+	    { 
+	       // printTheArray(arr, n); 
+	    	String newMask = Arrays.stream(arr)
+				    .mapToObj(x -> ((Integer) x).toString())
+				    .collect(Collectors.joining(""));
+	    	maskList.add(newMask);
+	        return; 
+	    } 
+	  
+	    if(mask[i].equals("0") ) {
+	    	arr[i] = 2;
+	    	generateAllBinaryStrings(mask, n, arr, i + 1, maskList);
+	    } else if(mask[i].equals("1")) {
+	    	arr[i] = 1;
+	    	generateAllBinaryStrings(mask, n, arr, i + 1, maskList);
+	    } else {
+		    // First assign "0" at ith position 
+		    // and try for all other permutations 
+		    // for remaining positions 
+		    arr[i] = 0; 
+		    generateAllBinaryStrings(mask, n, arr, i + 1, maskList); 
+		  
+		    // And then assign "1" at ith position 
+		    // and try for all other permutations 
+		    // for remaining positions 
+		    arr[i] = 1; 
+		    generateAllBinaryStrings(mask, n, arr, i + 1, maskList);
+	    }
+	} 
+	
+	public List<String> generateMasks(String theMask) {
+		int n = theMask.length(); 
+		  
+	    int[] arr = new int[n];
+	    List<String> maskList = new ArrayList<>();
+	    String[] mask = theMask.split("");
+	    generateAllBinaryStrings(mask, n, arr, 0, maskList);
+	    	    
+		return maskList;
+	}
 	public static void main(String[] args) {
 		Day14Brian b = new Day14Brian();
-		//b.testMask(333450);
-		b.solvePartOne();
-		// b.viewMask(0);
-		//b.viewMask(x);		
-		//b.viewMask(b.createMask(2));
+		//b.solvePartOne();
+		b.solvePartTwo();
+		//String theMask = "0101XX01X00X1X1011X1X000000101X10001";
+		//int[] results = b.generateMasks("00000");
 		
-		
-//		int x = 0;
-//		print("Before: " + x);
-//		b.viewMask(x);
-//		String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
-//		print(mask);
-//		int result = b.applyMask(x, mask);
-//		print("After: " + result);
-//		b.viewMask(result);
+//		int n = theMask.length(); 
+//		  
+//	    int[] arr = new int[n];
+//	    String[] mask = theMask.split("");
+//	    generateAllBinaryStrings(mask, n, arr, 0);
+	    System.out.println();
 	}
 }
